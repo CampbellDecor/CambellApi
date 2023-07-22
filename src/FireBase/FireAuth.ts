efb0ea794e55edecafe9bd2fbfc563814189b662
Mahendran Thanujan <73095554+ThanuMahee12@users.noreply.github.com>
2023-07-23 05:14:01 +0530

    updated

--- src/FireBase/FireAuth.ts
+++ src/FireBase/FireAuth.ts
@@ -1,5 +1,5 @@
 import fire from './Firebase';
-import {User} from '../Model/User'
+import { User } from '../Model/User';
 const Auth=fire.auth();
 export class FireAuth{
     static async add(user:User){
@@ -17,6 +17,27 @@ export class FireAuth{
 
         } catch (error) {
             throw error;
+        }
+    }
+    static async delete(user:string){
+        try {
+                await Auth.deleteUser(user);
+        } catch (error) {
+            throw error;
+        }
+    }
+    static async edit(user:User){
+        try {
+            if(user?.getUid()!=undefined)
+                await Auth.updateUser( user.getUid() as string,{
+                    displayName:user.getUsername(),
+                    password:user.getPassword(),
+                    email:user.getEmail(),
+                    phoneNumber:user.getMobile(),
+                    photoURL:user.getProfile()
+            });
+        } catch (error) {
+            
         }
     }
 }
