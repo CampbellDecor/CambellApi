c6e4d4b0bbe8f7aa6e3b473e6284541abee9423e
Mahendran Thanujan <73095554+ThanuMahee12@users.noreply.github.com>
2023-07-23 05:50:44 +0530

    testing

--- tests/firebase.test.ts
+++ tests/firebase.test.ts
@@ -7,10 +7,21 @@ describe("Testing with firebase ",()=>{
             
         })
       it("FireAuth connectivity test",()=>{
-
+            
         })
-        it("FireStore connectivity test",()=>{
+        it("FireStore connectivity test",async ()=>{
+            const collectionRef = fire.firestore().collection('test_collection');
+            const docRef = collectionRef.doc('test_doc');
+            await docRef.set({ testField: 'testValue' });
 
+            // Retrieve the test document
+            const snapshot = await docRef.get();
+         // Check if the test document has the expected data
+            expect(snapshot.exists).toBe(true);
+            expect(snapshot?.data()?.testField).toBe('testValue');
+            expect(snapshot.exists).toBeTruthy();
+            expect(collectionRef.count).toBe(1);
+           await  docRef.delete();
         })
         it("Realtime Database connectivity test",()=>{
 
