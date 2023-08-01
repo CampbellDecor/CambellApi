324396e730856c0f63f8224f465b35b2899f0f80
Mahendran Thanujan <73095554+ThanuMahee12@users.noreply.github.com>
2023-08-01 06:29:43 +0530

    updated

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,21 +1,38 @@
-import { Request,Response } from "express";
+import { Request,Response, response } from "express";
 import UserService from '../Service/userService';
 import UserModel,{UserBuilder} from '../Model/User'
+import {UserController} from './controller';
 const userService=new UserService();
 
 const user=new UserBuilder();
-export default class User{
-
-   addUser(req:Request,res:Response){
+export default class User extends UserController{
+    add(req:Request,res:Response):void{
         try {
-           const [email,mobile,password]= req.body;
-            user.setEmail(email)
-            .setMobile(mobile)
-            .setPassword(password);
-            userService.addService(user.getUser());          
+            const {email,password,mobile}=req.body;
+            user.setEmail(email).setPassword(password).setMobile(mobile)
         } catch (error) {
-            res.status(404).json(error);
+            response.status(404).json(error);
         }
-   }
+        
+        }
+    edit(req:Request,res:Response):void{
+
+    }
+    delete(req:Request,res:Response):void{
+
+    }
+    getAll(req:Request,res:Response):void{
+
+    }
+    getById(req:Request,res:Response):void{
+
+    }
+    login(req:Request,res:Response):void{
+
+    }
+    logout(req:Request,res:Response):void{
+
+    }
+  
 
 }
