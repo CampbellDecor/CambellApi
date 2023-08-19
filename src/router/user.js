ab10b816fc296a584a4f6c6d33b9ed9892d903a0
Mahendran Thanujan <73095554+ThanuMahee12@users.noreply.github.com>
2023-08-19 19:34:42 +0530

    updted api

--- src/router/user.js
+++ src/router/user.js
@@ -5,12 +5,7 @@ const userConttoller=new UserContoller();
 const Router=express.Router();
 Router.post("/add",userConttoller.add);
 Router.route( "/" )
-        .post( userConttoller.getAll );
-Router.route("/:id")
-        .delete(userConttoller.delete)
-        .put( userConttoller.edit )
-        .post(userConttoller.)
-        
-Router.post("/sigin",userConttoller.login);
-Router.post("/sigout",userConttoller.logout);
+        .post( UserContoller.getUsers);
+
+
 export default Router;
