8512d2bc4f573aa26e9937eb509090667c482b76
Mahendran Thanujan <73095554+ThanuMahee12@users.noreply.github.com>
2023-08-19 19:38:16 +0530

    router api changed

--- src/router/user.js
+++ src/router/user.js
@@ -1,9 +1,9 @@
 const express  =require( 'express');
 const UserContoller =require( '../controller/user');
-const userConttoller=new UserContoller();
 
 const Router=express.Router();
-Router.post("/add",userConttoller.add);
+
+// Router.post("/add",userConttoller.add);
 Router.route( "/" )
         .post( UserContoller.getUsers);
 
